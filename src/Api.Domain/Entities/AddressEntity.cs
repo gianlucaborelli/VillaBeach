a3e3905886f9dc4b37fa51b7e1220b103c6c735c
Gianluca Borelli <gianlucaborelli2@hotmail.com>
2023-04-01 02:01:24 -0300

    Create AddressEntity

---
+++ src/Api.Domain/Entities/AddressEntity.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Domain.Entities
+{
+    public class AddressEntity: BaseEntity
+    {
+        private string _postalCode;
+        public string PostalCode
+        {
+            get { return _postalCode; }
+            set { _postalCode = value; }
+        }
+        
+        private string _street;
+        public string Street
+        {
+            get { return _street; }
+            set { _street = value; }
+        }
+        
+        private string _number;
+        public string Number
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+
+        private string _district;
+        public string District
+        {
+            get { return _district; }
+            set { _district = value; }
+        }
+        
+        private string _city;
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+
+        private string _state;
+        public string State
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+        
+        private string _description;
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }   
+                
+        [ForeignKey("User")]
+        public UserEntity User{get;set;}
+        public Guid _userId;
+    }
+}
