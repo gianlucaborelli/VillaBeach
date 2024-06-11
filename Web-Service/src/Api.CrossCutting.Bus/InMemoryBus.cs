834485ebcf7d628265dcf09f38b0279683f281a0
Gianluca <gianlucaborelli2@hotmail.com>
2024-06-11 16:55:09 -0300

    Implemented logging on InMemoryBus

--- Web-Service/src/Api.CrossCutting.Bus/InMemoryBus.cs
+++ Web-Service/src/Api.CrossCutting.Bus/InMemoryBus.cs
@@ -3,6 +3,7 @@ using Api.Core.Events.Messaging;
 using Api.Core.Mediator;
 using FluentValidation.Results;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace Api.CrossCutting.Bus;
 
@@ -10,24 +11,43 @@ public sealed class InMemoryBus : IMediatorHandler
 {
     private readonly IMediator _mediator;
     private readonly IEventStore _eventStore;
+    private readonly ILogger<InMemoryBus> _logger;
 
-    public InMemoryBus(IEventStore eventStore, IMediator mediator)
+    public InMemoryBus(IEventStore eventStore, IMediator mediator, ILogger<InMemoryBus> logger)
     {
         _eventStore = eventStore;
         _mediator = mediator;
+        _logger = logger;
     }
 
     public async Task PublishEvent<T>(T @event) where T : Event
     {
-        if (!@event.MessageType.Equals("DomainNotification"))
-            _eventStore?.Save(@event);
+        try
+        {
+            if (!@event.MessageType.Equals("DomainNotification"))
+                _eventStore?.Save(@event);
 
-        await _mediator.Publish(@event);
+            await _mediator.Publish(@event);
+            _logger.LogInformation($"Published event of type {typeof(T).Name}");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Error publishing event of type {typeof(T).Name}");
+            throw;
+        }
     }
 
     public async Task<ValidationResult> SendCommand<T>(T command) where T : Command
     {
-        return await _mediator.Send(command);
+        try
+        {
+            return await _mediator.Send(command);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Error sending command of type {typeof(T).Name}");
+            throw;
+        }
     }
 
     Task<ValidationResult> IMediatorHandler.SendCommand<T>(T command)
