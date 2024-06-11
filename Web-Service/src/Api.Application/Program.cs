88a050639578b75dfaf0d310c2fadcdc8a98e53d
Gianluca <gianlucaborelli2@hotmail.com>
2024-06-11 13:15:59 -0300

    Implemented Staging Environment

--- Web-Service/src/Api.Application/Program.cs
+++ Web-Service/src/Api.Application/Program.cs
@@ -9,7 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.ConfigureAuthentication();
 builder.Services.ConfigureMapperService();
@@ -32,7 +31,7 @@ builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
 {
     app.UseSwagger();
     app.UseSwaggerUI(opt =>
@@ -41,16 +40,15 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-if (!app.Environment.IsDevelopment() && app.Environment.IsStaging())
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.Use(async (context, next) =>
 {
-    if (context.Request.Path == "/swagger" && app.Environment.IsDevelopment() && app.Environment.IsStaging())
+    if (context.Request.Path == "/swagger" && (app.Environment.IsDevelopment() || app.Environment.IsStaging()))
     {
         context.Response.Redirect("/swagger/index.html");
         return;
