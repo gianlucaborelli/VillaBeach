e82f85984d13be029e0b284c7269dfb2487c02bb
Gianluca <gianlucaborelli2@hotmail.com>
2024-06-12 01:52:54 -0300

    Fixed Dockerfile

--- Web-Service/src/Api.CrossCutting.Communication/Services/EmailService.cs
+++ Web-Service/src/Api.CrossCutting.Communication/Services/EmailService.cs
@@ -20,10 +20,7 @@ namespace Api.CrossCutting.Communication.Services
 
             var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
 
-            _templateDirectory = Path.Combine(baseDirectory, "Api.CrossCutting.Communication", "Templates");
-
-            if (!Directory.Exists(_templateDirectory))
-                throw new DirectoryNotFoundException($"O diretório de templates não foi encontrado: {_templateDirectory}");
+            _templateDirectory = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())!.FullName, "Api.CrossCutting.Communication", "Templates");
         }
 
         public async Task SendEmailVerification(string toEmail, string toName, string verificationLink)
